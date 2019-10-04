91b32bec20bd9398c150a6f5018558d521726640
Mark Geha <markgeha8@gmail.com>
2019-10-03 19:45:28 -0700

    Update

--- picCapture.py
+++ picCapture.py
@@ -1,18 +1,18 @@
 #Code gotten from: https://github.com/kevinam99/capturing-images-from-webcam-using-opencv-python/blob/master/webcam-capture-v1.01.py
 #Update
 
-import cv2 
+import cv2
 
-key = cv2. waitKey(1)
+#key = cv2. waitKey(1)
 webcam = cv2.VideoCapture(0)
-while True:
+#while True:
      
     check, frame = webcam.read()
     print(check) #prints true as long as the webcam is running
     print(frame) #prints matrix values of each framecd 
     cv2.imshow("Capturing", frame)
-    key = cv2.waitKey(1)
-    if key == ord('s'): 
+    #key = cv2.waitKey(1)
+    #if key == ord('s'): 
         cv2.imwrite(filename='saved_img.jpg', img=frame)
         webcam.release()
         img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
@@ -31,10 +31,10 @@ while True:
         print("Image saved!")
      
         break
-    elif key == ord('q'):
-        print("Turning off camera.")
-        webcam.release()
-        print("Camera off.")
-        print("Program ended.")
-        cv2.destroyAllWindows()
-        break
+    #elif key == ord('q'):
+    #    print("Turning off camera.")
+    #    webcam.release()
+    #    print("Camera off.")
+    #    print("Program ended.")
+    #    cv2.destroyAllWindows()
+    #    break
