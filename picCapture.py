3d2b425cf4b232b047f9c0e2c6a9e5da850f223b
Mark Geha <markgeha8@gmail.com>
2019-10-03 21:37:47 -0700

    Update

--- picCapture.py
+++ picCapture.py
@@ -1,40 +1,16 @@
-#Code gotten from: https://github.com/kevinam99/capturing-images-from-webcam-using-opencv-python/blob/master/webcam-capture-v1.01.py
-#Update
-
+import numpy as np
 import cv2
+from mss import mss
+from PIL import Image
 
-key = cv2.waitKey(1)
-webcam = cv2.VideoCapture(0)
-while True:
-     
-    check, frame = webcam.read()
-    print(check) #prints true as long as the webcam is running
-    print(frame) #prints matrix values of each framecd 
-    cv2.imshow("Capturing", frame)
-    key = cv2.waitKey(1)
-    if key == ord('s'): 
-        cv2.imwrite(filename='saved_img.jpg', img=frame)
-        webcam.release()
-        img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
-        img_new = cv2.imshow("Captured Image", img_new)
-        cv2.waitKey(1650)
-        cv2.destroyAllWindows()
-        print("Processing image...")
-        img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
-        print("Converting RGB image to grayscale...")
-        gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-        print("Converted RGB image to grayscale...")
-        print("Resizing image to 28x28 scale...")
-        img_ = cv2.resize(gray,(28,28))
-        print("Resized...")
-        img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
-        print("Image saved!")
-     
-        break
-    elif key == ord('q'):
-        print("Turning off camera.")
-        webcam.release()
-        print("Camera off.")
-        print("Program ended.")
+mon = {'top': 160, 'left': 160, 'width': 200, 'height': 200}
+
+sct = mss()
+
+while 1:
+    sct.get_pixels(mon)
+    img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
+    cv2.imshow('test', np.array(img))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
