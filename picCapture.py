6ee394fa72dca127a863aeae1dd54ec52ae80d77
Mark Geha <markgeha8@gmail.com>
2019-10-03 19:46:34 -0700

    Update

--- picCapture.py
+++ picCapture.py
@@ -3,9 +3,9 @@
 
 import cv2
 
-#key = cv2. waitKey(1)
+#key = cv2.waitKey(1)
 webcam = cv2.VideoCapture(0)
-#while True:
+while True:
      
     check, frame = webcam.read()
     print(check) #prints true as long as the webcam is running
