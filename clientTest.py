ae2db2aa5ac81dd450a7e77f196934f3e761b7b4
Mark Geha <markgeha8@gmail.com>
2019-10-10 16:59:55 -0700

    Updated clientTest.py

--- clientTest.py
+++ clientTest.py
@@ -1,6 +1,6 @@
 #Client test
 import socket
-client = socet.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('0.0.0.0', 8080))
 client.send("I am CLIENT\n")
 from_server = client.recv(4096)
